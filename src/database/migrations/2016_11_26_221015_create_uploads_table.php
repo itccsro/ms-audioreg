4b64104ad330b9102d4cbf16b8f09a83e92e98dc
Lucian Stuparu <lucianstuparu@gmail.com>
2016-11-27 00:40:42 +0200

    Add upload model and migration

---
+++ src/database/migrations/2016_11_26_221015_create_uploads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUploadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->nullable();
+            $table->string('path', 200)->nullable();
+            $table->smallInteger('ignored_patients')->nullable();
+            $table->smallInteger('ignored_tests')->nullable();
+            $table->smallInteger('valid_patients')->nullable();
+            $table->smallInteger('valid_tests')->nullable();
+            $table->nullableTimestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uploads');
+    }
+}
